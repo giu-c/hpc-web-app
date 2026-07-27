47c90cd4035b1ce54cf59a3956449dd2078cceb3
giu-c <giusecurridori@gmail.com>
2026-07-27 14:23:21 +0200

    ...

--- streamlit_app.py
+++ streamlit_app.py
@@ -423,8 +423,8 @@ def build_payload(data, groups, city_ids, arch_values, vcpu, ram,
 # ---------------------------- UI (Streamlit) ---------------------------------
 
 st.set_page_config(
-    page_title="HPC -  🟠AWS vs OCI🔴 ​",
-    page_icon="☁️​💲🥊",
+    page_title="HPC -  🟠AWS ⚔️​ OCI🔴",
+    page_icon="☁️​",
     layout="wide",
     initial_sidebar_state="expanded",
 )
